faca280be3a5b3fd3d60838dbb1980eac48f7ad4
Ty Thorsen <thorsenty@gmail.com>
2014-04-08 21:47:18 -0500

    add top-nav and smooth scrolling to page shell

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//Add global Bird Ultimate js code here.
+var BU = {
+
+	HEADER_OFFSET: 65,
+		
+	events: {
+
+		scrollToAnchor: function(anchor) {
+			if (location.pathname.replace(/^\//,'') == anchor.pathname.replace(/^\//,'') 
+	            || location.hostname == anchor.hostname) {
+	          var target = $(anchor.hash);
+	          target = target.length ? target : $('[name=' + anchor.hash.slice(1) +']');
+	          if (target.length) {
+	            $('html,body').animate({
+	              scrollTop: target.offset().top - BU.HEADER_OFFSET
+	            }, 1000);
+	            return false;
+	          }
+	        }
+		}
+
+	},
+
+	init: function() {
+		$('a[href*=#]:not([href=#])').on("click", function(event) {
+			event.preventDefault();
+			BU.events.scrollToAnchor(this);
+		});
+	}	
+};
+
+$(document).ready(function() {
+	BU.init();
+});
